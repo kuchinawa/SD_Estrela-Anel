4957f6b166e9cfd2ba0f5c45c1f8ffc0795a6ffe
Kuchinawa_ <davi.clemente@alunos.ufersa.edu.br>
2024-02-28 20:02:29 -0300

    pratica 3.5 quase dando certo
    Signed-off-by: Kuchinawa_ <davi.clemente@alunos.ufersa.edu.br>

---
+++ src/anel/Cliente.java
@@ -0,0 +1,33 @@
+package anel;
+
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Cliente {
+    Socket socket;
+    InetAddress inet;
+    String ip;
+    int porta;;
+    public Cliente(String ip, int porta) {
+        this.ip = ip;
+        this.porta = porta;
+        this.rodar();
+    }
+
+    private void rodar(){
+        try {
+            socket = new Socket(ip, porta);
+            inet = socket.getInetAddress();
+            System.out.println("HostAddress = " + inet.getHostAddress());
+            System.out.println("HostName = " + inet.getHostName());
+            ImplCliente c = new ImplCliente(socket);
+            Thread t = new Thread(c);
+            t.start();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        new Cliente("127.0.0.1", 54321);
+    }
+}
