6b03e192404f0b976acd67342eee6625283e9fa8
Kuchinawa_ <davi.clemente@alunos.ufersa.edu.br>
2024-03-06 01:59:09 -0300

    finguindo que comecei o estrela
    Signed-off-by: Kuchinawa_ <davi.clemente@alunos.ufersa.edu.br>

--- src/estrela/control/ClienteServidor.java
+++ src/estrela/control/ClienteServidor.java
@@ -1,4 +1,12 @@
 package estrela.control;
 
+import estrela.model.Servidor;
+import estrela.view.Cliente;
+
 public class ClienteServidor {
+    public static void main(String[] args) {
+        int portaServidor = 5000;
+
+        int portaCliente = 5001;
+    }
 }
