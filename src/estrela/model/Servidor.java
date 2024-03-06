556ddbbed66eef1888287ef08cf53292872bd4d2
Kuchinawa_ <davi.clemente@alunos.ufersa.edu.br>
2024-03-06 02:22:21 -0300

    tá, algo aconteceu
    Signed-off-by: Kuchinawa_ <davi.clemente@alunos.ufersa.edu.br>

--- src/estrela/model/Servidor.java
+++ src/estrela/model/Servidor.java
@@ -1,4 +1,77 @@
 package estrela.model;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class Servidor {
+    private final int porta;
+    private final List<Socket> clientes;
+
+    public Servidor(int porta) {
+        this.porta = porta;
+        this.clientes = new ArrayList<>();
+    }
+
+    public void iniciar() {
+        try (ServerSocket serverSocket = new ServerSocket(porta)) {
+            System.out.println("Servidor iniciado na porta " + porta);
+
+            while (true) {
+                Socket clienteSocket = serverSocket.accept();
+                clientes.add(clienteSocket);
+                System.out.println("Novo cliente conectado: " + clienteSocket);
+
+                Thread clienteThread = new Thread(new ClienteHandler(clienteSocket));
+                clienteThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class ClienteHandler implements Runnable {
+        private final Socket clienteSocket;
+
+        public ClienteHandler(Socket clienteSocket) {
+            this.clienteSocket = clienteSocket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                BufferedReader entrada = new BufferedReader(new InputStreamReader(clienteSocket.getInputStream()));
+
+                String mensagem;
+                while ((mensagem = entrada.readLine()) != null) {
+                    System.out.println("Mensagem recebida de " + clienteSocket + ": " + mensagem);
+                    encaminharMensagem(mensagem);
+                }
+
+                entrada.close();
+                clientes.remove(clienteSocket);
+                clienteSocket.close();
+                System.out.println("Cliente desconectado: " + clienteSocket);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void encaminharMensagem(String mensagem) {
+            for (Socket cliente : clientes) {
+                try {
+                    PrintWriter saida = new PrintWriter(cliente.getOutputStream(), true);
+                    saida.println(mensagem);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Servidor servidor = new Servidor(5000);
+        servidor.iniciar();
+    }
 }
+
--- src/estrela/view/Cliente.java
+++ src/estrela/view/Cliente.java
@@ -1,4 +1,58 @@
 package estrela.view;
 
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
 public class Cliente {
+    public static void main(String[] args) {
+        final String HOST = "localhost";
+        final int PORTA = 5000;
+
+        try (Socket socket = new Socket(HOST, PORTA)) {
+            System.out.println("Conectado ao servidor");
+
+            Thread receberThread = new Thread(new ReceberMensagens(socket));
+            receberThread.start();
+
+            PrintWriter saida = new PrintWriter(socket.getOutputStream(), true);
+            Scanner scanner = new Scanner(System.in);
+
+            String mensagem;
+            do {
+                System.out.print("Digite a mensagem: ");
+                mensagem = scanner.nextLine();
+                saida.println(mensagem);
+            } while (!mensagem.equalsIgnoreCase("fim"));
+
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class ReceberMensagens implements Runnable {
+        private final Socket socket;
+
+        public ReceberMensagens(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                String mensagem;
+                while ((mensagem = entrada.readLine()) != null) {
+                    System.out.println("Mensagem recebida do servidor: " + mensagem);
+                }
+
+                entrada.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
